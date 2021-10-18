c6c5f617e211ff3d2d9043f8471a53a3a3376b1f
Marcio Cruz de Almeida <67694075+marciocadev@users.noreply.github.com>
2021-10-18 10:05:45 -0300

    Update lambda.ts

--- typescript/sam-cdk-example/lambda-fns/lambda.ts
+++ typescript/sam-cdk-example/lambda-fns/lambda.ts
@@ -1,9 +1,15 @@
 const axios = require('axios');
 const AWSXRay = require('aws-xray-sdk');
 const AWS = require('aws-sdk');
+//const AWS = AWSXRay.captureAWS(require('aws-sdk'));
 const db = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'})
 
 export async function handler(event: AWSLambda.APIGatewayEvent, context: AWSLambda.Context) {
+    
+    // AWS.captureFunc('annotations', function(subsegment: any){
+    //     subsegment.addAnnotation('Name', "teste");
+    //     subsegment.addAnnotation('UserID', event.userid);
+    //   });
 
     const res = await axios.get('https://httpbin.org/get', {
         params: {
